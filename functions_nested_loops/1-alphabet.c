6f84657f7366dd4de7429a50c45ee726f2c8ba00
root <root@LAPTOP-FAU10JKU>
2025-02-21 18:30:50 +0100

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - check the code
+ *
+ *Return: Always 0
+ */
+void print_alphabet(void)
+{
+int print_alphabet;
+for (print_alphabet = 97; print_alphabet <= 122; print_alphabet++)
+{
+_putchar(print_alphabet);
+}
+_putchar('\n');
+}
