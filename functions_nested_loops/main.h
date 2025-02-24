c5c0dd7e0f090130563dc86426f30d408ea5ebfe
root <root@LAPTOP-FAU10JKU>
2025-02-24 11:50:22 +0100

    tt

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _putchar(char c);
+
 #endif /* MAIN_H */
 
