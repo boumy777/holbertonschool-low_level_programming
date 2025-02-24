047034b57ee387341ad1cf7ddfbb335314a95abc
root <root@LAPTOP-FAU10JKU>
2025-02-24 12:23:04 +0100

    tt

--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
- * _islower - Checks if a character is lowercase.
- * @c: The character to check.
- *
- * Return: 1 if c is lowercase, 0 otherwise.
+ * _islower - Entry Point
+ *@c: character to be verified
+ * Return: Always 0.
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	return (0);
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
+
 
 
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,6 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdio.h>
 
-int _islower(int c);  // Déclaration correcte de la fonction
-
-#endif
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c;
+c = 'a';
+printf("%c: %d\n", c, _islower(c));
+c = 'A';
+printf("%c: %d\n", c, _islower(c));
+return (0);
+}
