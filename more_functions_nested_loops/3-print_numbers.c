4c8a873858295f7b199321f2db4d4d6926f24d13
root <root@LAPTOP-FAU10JKU>
2025-02-25 15:40:34 +0100

    task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Affiche les chiffres de 0 à 9 suivis d'un saut de ligne
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0'); /* Convertit le chiffre en caractère */
+	_putchar('\n');
+}
+
