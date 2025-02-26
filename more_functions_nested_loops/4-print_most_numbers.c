11c2fe48534f21ed71ba8ca4bf88626957329b20
root <root@LAPTOP-FAU10JKU>
2025-02-26 10:38:59 +0100

    task4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,12 @@
 
 void print_most_numbers(void)
 {
-    char numbers[] = "01356789\n";
-    char *ptr = numbers;
+	char numbers[] = "01356789\n";
+	char *ptr = numbers;
 
-    while (*ptr)
-    {
-        _putchar(*ptr);
-        ptr++;
-    }
+	while (*ptr)
+	{
+		_putchar(*ptr);
+		ptr++;
+	}
 }
-
