6b92473992b73bfc50e64f1e80ee1be8a83d48f8
root <root@LAPTOP-FAU10JKU>
2025-02-26 10:54:52 +0100

    task4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - Affiche les chiffres de 0 à 9 sauf 2 et 4
+ *
+ * Description: Cette fonction utilise `_putchar` pour afficher
+ * les chiffres de 0 à 9, en sautant 2 et 4, suivis d'un retour à la ligne.
+ * Elle respecte la contrainte d'utiliser `_putchar` seulement deux fois.
+ */
 void print_most_numbers(void)
 {	char numbers[] = "01356789\n";
 	char *ptr = numbers;
