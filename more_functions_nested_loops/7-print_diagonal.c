f72b25ce6bb3d9c36908ed19cac07e11711ed2bc
root <root@LAPTOP-FAU10JKU>
2025-02-26 12:52:39 +0100

    task7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,9 +4,8 @@
  * print_diagonal - Dessine une ligne diagonale sur le terminal
  * @n: Nombre de fois que le caractère '\' doit être imprimé
  *
- * Description: Si n est supérieur à 0, la fonction affiche une
- * diagonale en décalant chaque '\' d'un espace supplémentaire.
- * Si n est 0 ou moins, elle imprime seulement un retour à la ligne.
+ * Description: Si n est supérieur à 0, la fonction affiche une diagonale
+ * Si n est 0 ou moins, la fonction imprime uniquement un retour à la ligne.
  */
 void print_diagonal(int n)
 {	int i, j;
@@ -16,11 +15,13 @@ void print_diagonal(int n)
 		_putchar('\n');
 		return;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)  /* Pour chaque ligne */
 	{
-		for (j = 0; j < i; j++)
-			_putchar(' '); /* Ajoute les espaces pour le décalage */
-		_putchar('\\'); /* Affiche le caractère '\' */
-		_putchar('\n'); /* Passe à la ligne suivante */
+		for (j = 0; j < i; j++)  /* Affiche des espaces avant '\' */
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');  /* Affiche le caractère '\' */
+		_putchar('\n');
 	}
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,5 +3,5 @@
 
 void print_line(int n);
 int _putchar(char c);
-
+void print_diagonal(int n);
 #endif /* MAIN_H */
