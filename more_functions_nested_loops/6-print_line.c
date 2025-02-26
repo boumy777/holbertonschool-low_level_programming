852e243e185c93a99805c7fb10d9b193f2af6329
root <root@LAPTOP-FAU10JKU>
2025-02-26 11:59:57 +0100

    task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Dessine une ligne droite sur le terminal
+ * @n: Nombre de fois que le caractère _ doit être imprimé
+ *
+ * Description: Si n est supérieur à 0, la fonction affiche
+ * une ligne droite avec le caractère '_'. Si n est 0 ou moins,
+ * la fonction imprime seulement un retour à la ligne.
+ */
+void print_line(int n)
+{	 int i;  /* Déclare la variable i avant la boucle*/
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)  /* Utilise la variable i dans la boucle*/
+				 
+	{
+	_putchar('_'); /* Affiche le caractère _ */
+	}
+	_putchar('\n'); /* Ajoute le retour à la ligne à la fin */
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void more_numbers(void);
+void print_diagonal(int n);
 int _putchar(char c);
 
 #endif /* MAIN_H */
-
