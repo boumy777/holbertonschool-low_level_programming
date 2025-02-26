7f37b9cc6790ed3d995669f75923d2d4343ea381
root <root@LAPTOP-FAU10JKU>
2025-02-26 11:14:45 +0100

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,27 @@
  *
  * Description: Cette fonction affiche les nombres de 0 à 14
  * sur 10 lignes, avec un retour à la ligne après chaque série.
+ * Elle utilise `_putchar` seulement 3 fois.
  */
 void more_numbers(void)
-{	int i, j;
+{
+    char numbers[] =
+        "01234567891011121314\n"
+        "01234567891011121314\n"
+        "01234567891011121314\n"
+        "01234567891011121314\n"
+        "01234567891011121314\n"
+        "01234567891011121314\n"
+        "01234567891011121314\n"
+        "01234567891011121314\n"
+        "01234567891011121314\n"
+        "01234567891011121314\n";
 
-	for (i = 0; i < 10; i++)  /* Boucle pour 10 lignes */
-	{
-		for (j = 0; j <= 14; j++)  /* Boucle pour 0 à 14 */
-		{
-			if (j >= 10)  /* Si le nombre a deux chiffres, afficher le premier */
-				_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');  /* Afficher le chiffre des unités */
-		}
-		_putchar('\n');  /* Nouvelle ligne après chaque série */
-	}
+    char *ptr = numbers;
+
+    while (*ptr)
+    {
+        _putchar(*ptr);
+        ptr++;
+    }
 }
