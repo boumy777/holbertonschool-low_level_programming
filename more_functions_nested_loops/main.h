92e01dab3aef1bbd2a2a49c853710095f75e66aa
root <root@LAPTOP-FAU10JKU>
2025-02-27 14:51:57 +0100

    task9

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
 
-void print_line(int n);
-int _putchar(char c);
+int main(void)
+{
+    // Ton code ici
+    return (0);
+}
 
-#endif /* MAIN_H */
