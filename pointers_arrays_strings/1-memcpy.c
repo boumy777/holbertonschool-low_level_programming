7db2193436aa7ef3bcca21af8762917fdfa37506
root <root@LAPTOP-FAU10JKU>
2025-03-04 12:27:05 +0100

    task1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copie une zone mémoire
+ * @dest: Pointeur vers la destination
+ * @src: Pointeur vers la source
+ * @n: Nombre d'octets à copier
+ *
+ * Return: Pointeur vers `dest`
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest); /* Parenthèses ajoutées pour respecter Betty */
+}
