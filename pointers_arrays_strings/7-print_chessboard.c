01873985500fcfca5494535df579f39ae45a93cd
root <root@LAPTOP-FAU10JKU>
2025-03-05 12:58:48 +0100

    task7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,30 +1,39 @@
-#include <stdio.h> /*Inclusion de stdio.h pour printf*/
+#include <stdio.h>
 
+/**
+ * print_chessboard - Affiche un échiquier 8x8.
+ * @a: Tableau 2D contenant l'échiquier.
+ */
 void print_chessboard(char (*a)[8])
+{	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
 {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            printf("%c ", a[i][j]); /* Affiche chaque caractère suivi d'un espace*/
-        }
-        printf("\n"); /* Nouvelle ligne après chaque rangée*/
-    }
-}
 
-int main()
-{
-    char board[8][8] = {
-        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
-    };
+/**
+ * main - Fonction principale pour tester print_chessboard.
+ * Return: 0 si succès.
+ */
+int main(void)
+{	char board[8][8] = {
+	{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+	{'.', '.', '.', '.', '.', '.', '.', '.'},
+	{'.', '.', '.', '.', '.', '.', '.', '.'},
+	{'.', '.', '.', '.', '.', '.', '.', '.'},
+	{'.', '.', '.', '.', '.', '.', '.', '.'},
+	{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+	{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+	};
 
-    print_chessboard(board);
-
-    return 0;
+	print_chessboard(board);
+	return (0);
 }
 
