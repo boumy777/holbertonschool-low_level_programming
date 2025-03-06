322692fb78734a69c3ec41b4cc0d83866d61596a
root <root@LAPTOP-FAU10JKU>
2025-03-06 11:33:24 +0100

    task0

---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+fichier README.md dans le dossier reccussion
---
+++ recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *s);  /* Function prototype for _puts_recursion */
+int _putchar(char c);           /* Function prototype for _putchar */
+
+#endif
