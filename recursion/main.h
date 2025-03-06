d5578f5d7b3190a88b4a6a0e437df2041c1b2a66
root <root@LAPTOP-FAU10JKU>
2025-03-06 12:04:50 +0100

    task5

--- recursion/main.h
+++ recursion/main.h
@@ -3,6 +3,6 @@
 
 void _puts_recursion(char *s);  /* Function prototype for _puts_recursion */
 int _putchar(char c);           /* Function prototype for _putchar */
-int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 
 #endif
