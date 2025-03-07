d9ad4f41bc28cdfe784d1527261c93139b25d746
root <root@LAPTOP-FAU10JKU>
2025-03-07 11:19:32 +0100

    task6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _is_prime_helper - helper function to check if a number is prime
+ * @n: number to check
+ * @i: iterator
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int _is_prime_helper(int n, int i)
+{
+	if (i == n)
+		return (1);
+	if (n % i == 0 || n < 2)
+		return (0);
+	return (_is_prime_helper(n, i + 1));
+}
+
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (_is_prime_helper(n, 2));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -4,5 +4,6 @@
 void _puts_recursion(char *s);  /* Function prototype for _puts_recursion */
 int _putchar(char c);           /* Function prototype for _putchar */
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 #endif
