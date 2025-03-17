54b572e656b0c58e1b63665c0e7653c540963a8f
root <root@LAPTOP-FAU10JKU>
2025-03-17 14:08:39 +0100

    task1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stddef.h>
+
 /**
  * struct dog - Structure représentant un chien
  * @name: Nom du chien
  * @age: Âge du chien
- * @owner: Nom du propriétaire du chien
+ * @owner: Propriétaire du chien
  */
-struct dog
+typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-};
+char *name;
+float age;
+char *owner;
+} dog_t;
 
+/* Prototypes des fonctions */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner); /* Ajout de la déclaration */
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
