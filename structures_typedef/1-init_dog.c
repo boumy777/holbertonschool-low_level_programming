c83174089bddf7f10b733dd9f1227e3c8f4e25e2
root <root@LAPTOP-FAU10JKU>
2025-03-17 12:33:28 +0100

    task1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,23 +1,22 @@
-#include <stddef.h>  /* Pour NULL */
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
+
+#include <stddef.h> /* Pour NULL */
 
 /**
- * init_dog - Initialize a variable of type struct dog.
- * @d: Pointer to the dog structure to be initialized.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Name of the dog's owner.
- *
- * Description: This function initializes the members of the dog structure
- * with the values provided as arguments.
+ * struct dog - Structure représentant un chien
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Propriétaire du chien
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d != NULL) /* Vérifie que le pointeur d existe */
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+    char *name;
+    float age;
+    char *owner;
+};
 
+/* Déclaration correcte de la fonction init_dog */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
+
 /**
  * struct dog - Structure représentant un chien
- * @name: Nom du chien (chaîne de caractères)
- * @age: Âge du chien (flottant)
- * @owner: Propriétaire du chien (chaîne de caractères)
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Propriétaire du chien
  */
-struct dog
+typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-};
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner); /* Ajout de la déclaration */
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
 
